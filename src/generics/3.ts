c7e41711d54fa7c80a3a179f794ab6f6b2e09d57
NataliaGurova <nyxnata@ukr.net>
2024-05-17 14:06:50 +0300

    update

--- src/generics/3.ts
+++ src/generics/3.ts
@@ -1,7 +1,6 @@
 
-
-function merge<T extends object, U extends object>(objA: T, objB: U) {
-  return Object.assign(objA, objB);
+function merge<T extends object, U extends object>(objA: T, objB: U): T & U {
+  return Object.assign({}, objA, objB);
 }
 
 const merged = merge({ name: 'Alisa' }, 'TEXT');
